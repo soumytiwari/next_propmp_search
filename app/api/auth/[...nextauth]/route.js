787c05756ed93d17aa244cd113cf89c33f1a1260
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-07 02:20:30 +0530

    created route.js and initialized google-auth credentials

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google';
+
+// we're using google providers here..
+const handler = NextAuth({
+    // options objects
+    providers: [                    //  array
+        GoogleProvider({
+            // clientId: '',
+            // clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+
+
+    // functions
+    // here we gonna get user session
+    async session({ session }) {
+
+    },
+    // here we gonna get user profile
+    async sigIn({ profile }) {
+
+    }
+})
+
+// generally we do only either 'GET'/'POST' but in next authentication we do it this way
+export { handler as GET, handler as POST };
