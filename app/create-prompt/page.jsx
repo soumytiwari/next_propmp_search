662b2eaa373190392cc99cc5035816131e45db48
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-09 17:43:09 +0530

    added form functionality for post

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+
+  // all the variables that we can pass to our form
+  // are we submitting our state?
+  const [submitting, setSubmitting] = useState(false);
+  // the post state with objects
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  // function to create prompt
+  const createPrompt = async (e) => {
+
+  }
+
+
   return (
-    <div>
-      CreatePrompt
-    </div>
+    <Form
+    type="Create"
+    post={post}
+    setPost={setPost}
+    submitting={submitting}
+    handleSubmit={createPrompt}
+    />
   )
 }
 
