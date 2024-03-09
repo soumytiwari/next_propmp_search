8b995ebaec1a9313a833c753557dc72ee998bbe1
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-09 17:56:29 +0530

    created my own api to create post

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -19,7 +19,32 @@ const CreatePrompt = () => {
 
   // function to create prompt
   const createPrompt = async (e) => {
+    // this prevents the default behaviour of event i.e., by doing page-reload. we wanna reduce number of reloads possible so..
+    e.preventDefault();
+    setSubmitting(true);        //  we can use this as some sort of loader later on
 
+    try{
+      // -------------------  api-call  -----------------------
+      // we're creating our own api call to do, to create post, to submit it
+      const response = await fetch('/api/prompt/new', {
+        // we're passing all the following data to the above '/api/prompt/new (this is the api we wanna call)' place.. using this #POST method
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      }) 
+      // ------------------ api-call  ------------------------
+
+      if(response.ok) {
+        router.push('/');
+      }
+    } catch(error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   }
 
 
