a03d9992e446dc9fd9ed7cbbd5616a07fb2d8d2c
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-09 18:00:16 +0530

    create my own route <shell>

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,4 @@
+// how to create new route?-> export const (specify the route type, here, POST) async(req, res) => { // * right your code.. right here * // } 
+export const POST = async(req, res) => {
+
+}
