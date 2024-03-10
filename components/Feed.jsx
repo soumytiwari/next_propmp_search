94d1aa8960f2c9468eb41912b500659f2d27aef7
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-10 16:17:03 +0530

    created feed sheell

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return (
-    <div>
-      Feed
-    </div>
-  )
-}
 
-export default Feed
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  return (
+    <section className="feed">
+      {/* /form for the search of our feed */}
+      <form className="relative w-full flex-center">
+        <input 
+        type="text" 
+        placeholder="Search for a tag or a username" 
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className="search_input peer"
+        />
+      </form>
+
+      {/* <PromptCardList 
+      data: /> */}
+    </section>
+  );
+};
+
+export default Feed;
