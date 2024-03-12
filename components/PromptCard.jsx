bc1be6c8e4ccbcc4314592d0cc32f5ed31aa1bfd
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-13 01:13:19 +0530

    rendered edit delete, CRUD operation

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -11,6 +11,10 @@ import { usePathname, useRouter } from "next/navigation";
 //
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   // function to copy the prompt to the clip-board
@@ -18,7 +22,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(""), 3000);
-  }
+  };
 
   return (
     <div className="prompt_card">
@@ -65,6 +69,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
